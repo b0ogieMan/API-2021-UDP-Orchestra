55184e5a54c6066d005e0a3b8ddd2a5dd6586895
Anthony Coke <anthony.coke@heig-vd.ch>
2022-02-02 18:20:40 +0100

    fix: now working, problem with object disassembly

--- docker/image-auditor/src/app.js
+++ docker/image-auditor/src/app.js
@@ -24,34 +24,31 @@ udpServer.bind(protocol.PORT, () => {
     udpServer.addMembership(protocol.MULTICAST_ADDRESS);
 });
 
-udpServer.on('message', function(msg, source) {
+udpServer.on('message', function(datagram, source) {
 
     // parse received data
-    const obj = JSON.parse(msg);
+    const musician = JSON.parse(datagram);
 
-    // create new json object and add time property
-    const musician = {
-        obj,
-        lastActive: Date.now(),
-    };
+    // store date when musician was last active, same operation for both cases
+    musician.lastActive = Date.now()
 
-    // find instrument associated with sound
-    musician.instrument = Object.keys(protocol.INSTRUMENTS)
-        .find((instrument) => protocol.INSTRUMENTS[instrument] === musician.sound);
-
-    // if in the map 
+    // if in the map
     if (activeMusicians.has(musician.uuid)) {
         musician.activeSince = activeMusicians.get(musician.uuid).activeSince; // no
         // need to update activeSince
     } else {
         musician.activeSince = musician.lastActive; // new element so we need to add
-        // a value to store the first 
+        // a value to store the first
     }
+
+    // find instrument associated with sound
+    musician.instrument = Object.keys(protocol.INSTRUMENTS)
+        .find((instrument) => protocol.INSTRUMENTS[instrument] === musician.sound);
 
     // Add or update the element with uuid === data.uuid
     activeMusicians.set(musician.uuid, musician);
 
-    console.log('New datagram has arrived: ' + msg + ' from source port: ' + source.port);
+    console.log('New datagram has arrived: ' + datagram + ' from source port: ' + source.port);
 });
 
 tcpServer.listen(TCP_PORT, function() {
@@ -59,19 +56,27 @@ tcpServer.listen(TCP_PORT, function() {
 });
 
 tcpServer.on('connection', function(socket) {
+
     const content = Array.from(activeMusicians.entries());
+
     const lastActive = content.filter(([uuid, musician]) => {
+
+        // using a predicate to check if musician has to be removed or not
         const active = Date.now() - musician.lastActive <= INTERVAL;
         if(!active)
             activeMusicians.delete(uuid);
         return active;
+
     })
-        .map(([uuid, musician]) => ({
+        .map(([uuid, musician]) => ({ // create a new map from the updated
+            // content
             uuid,
-            "instrument": musician.instrument,
-            "activeSince": new Date(musician.activeSince)
+            instrument: musician.instrument,
+            activeSince: new Date(musician.activeSince)
         }));
 
+
+    // send the map to the client
     socket.write(JSON.stringify(lastActive));
     socket.end();
 });
--- docker/image-musician/src/app.js
+++ docker/image-musician/src/app.js
@@ -1,4 +1,3 @@
-// UDP SERVER
 const protocol = require('./protocol');
 const udp = require('dgram');
 const { randomUUID } = require('crypto');
@@ -6,25 +5,30 @@ const SEND_INTERVAL = 1000;
 const client = udp.createSocket('udp4');
 const uuid = randomUUID();
 
+// check args
 if(process.argv.length !== 3){
     console.error("An instrument must be provided as an argument")
     process.exit(1);
 }
+// get instrument
 const instrument = process.argv[2];
 
+// throw error if not found
 if(protocol.INSTRUMENTS[instrument] === undefined){
     console.error("The instrument '" + instrument + "' is unknown")
     process.exit(1);
 }
 
+// send sound
 const sendSoundInterval = setInterval(() => sendSound(), SEND_INTERVAL);
 
+// generate payload
 const payload = JSON.stringify({
     uuid,
     sound : protocol.INSTRUMENTS[instrument],
 });
 
-
+// send sound
 function sendSound() {
     client.send(new Buffer(payload), protocol.PORT, protocol.MULTICAST_ADDRESS, (error => {
         if(error) {
